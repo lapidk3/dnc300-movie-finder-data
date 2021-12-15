ad5af9065a94cda9da5ec98111b59da45c811b04
Kenneth David <lapidk3@yahoo.com>
2021-12-15 09:08:13 -0800

    Finished controller

--- dnc-300-movie-finder-data/Controllers/MovieFinderController.cs
+++ dnc-300-movie-finder-data/Controllers/MovieFinderController.cs
@@ -1,18 +1,61 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Net;
+using System.Net.Http;
 using System.IO;
 
 namespace dnc_300_movie_finder_data.Controllers
 {
     public class MovieFinderController : Controller
     {
-        public string FindMovie()
+        private const string API_KEY = "8d062334";
+        private Hashtable movies;
+
+
+        public MovieFinderController()
         {
-            return "";
+            movies = new Hashtable();
         }
+
+        public string FindMovie(string param, string value)
+        {
+            string url = "";
+            string json = "";
+
+            // Check if user query will be an IMBD ID or a movie title
+            switch(param)
+            {
+                case "i":
+                    url = $"http://omdbapi.com/?i={value}&apikey={API_KEY}";
+                    break;
+                case "t":
+                    url = $"http://omdbapi.com/?t={value}&apikey={API_KEY}";
+                    break;
+            }
+
+            // Check if requested movie is in our cache, if so return the data
+            if (movies.ContainsKey(url))
+            {
+                return (string) movies[url];
+            }
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    json = client.DownloadString(url);
+                }
+
+            } catch(Exception ex){
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            return json;
+        }
+
     }
 }
--- dnc-300-movie-finder-dataTests/Controllers/MovieFinderControllerTests.cs
+++ dnc-300-movie-finder-dataTests/Controllers/MovieFinderControllerTests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generi,sc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,14 +14,14 @@ namespace dnc_300_movie_finder_data.Controllers.Tests
         public void TestFindMovieByTitle()
         {
             MovieFinderController mfc = new MovieFinderController();
-            string response = mfc.FindMovie(t: "Batman");
+            string response = mfc.FindMovie("t", "Batman");
             Assert.IsTrue(response.Contains("Batman"));
         }
         [TestMethod]
         public void TestFindMovieByTitleWithCache()
         {
             MovieFinderController mfc = new MovieFinderController();
-            string response = mfc.FindMovie(t: "baby%20driver");
+            string response = mfc.FindMovie("t", "baby%20driver");
             response = mfc.FindMovie(t: "baby%20driver");
             Assert.IsTrue(response.Contains("Baby Driver"));
         }
@@ -29,15 +29,15 @@ namespace dnc_300_movie_finder_data.Controllers.Tests
         public void TestFindMovieByID()
         {
             MovieFinderController mfc = new MovieFinderController();
-            string response = mfc.FindMovie(i: "tt3896198");
+            string response = mfc.FindMovie("i", "tt3896198");
             Assert.IsTrue(response.Contains("Guardians of the Galaxy"));
         }
         [TestMethod]
         public void TestFindMovieByIDWithCache()
         {
             MovieFinderController mfc = new MovieFinderController();
-            string response = mfc.FindMovie(i: "tt3896198 ");
-            response = mfc.FindMovie(i: "tt3896198 ");
+            string response = mfc.FindMovie("i", "tt3896198 ");
+            response = mfc.FindMovie("i", "tt3896198 ");
             Assert.IsTrue(response.Contains("Guardians of the Galaxy"));
         }
     }
